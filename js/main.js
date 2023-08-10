abc44d1f2fe0be5e67672f4fe8adb507fef74144
VEYMAY <veymayuz@gmail.com>
2023-08-10 15:47:57 +0500

    newww

--- js/main.js
+++ js/main.js
@@ -11,42 +11,40 @@ let checkout = document.querySelector('ul.checkout')
 
 let checkoutList = JSON.parse(localStorage.getItem('checkoutList'))?JSON.parse(localStorage.getItem('checkoutList')):[]
 
+let totalPrice = localStorage.getItem('totalPrice') ? localStorage.getItem('totalPrice') : 0
 
 overlay.addEventListener('click', () => {
   cartContent.classList.toggle('active')
   overlay.classList.toggle('active')
 })
+
 cart.addEventListener('click', () => {
   cartContent.classList.toggle('active')
   document.querySelector('.product_bar').style.boxShadow='none'
   overlay.classList.toggle('active')
 })
-let totalPrice = 0
-function showCheckout(id) {
-  checkoutList.forEach(datas => {
-    checkout.innerHTML += `
-      <li>
+
+
+products.forEach((item, id) => {
+  
+  function showCheckout() {
+    checkout.innerHTML = ''
+    checkoutList.forEach(datas => {
+      checkout.innerHTML += `
+        <li>
         <img
-          src="${datas.image}" alt="">
-        <div class="data">
+            src="${datas.image}" alt="">
+          <div class="data">
           <div class="name">${datas.name}</div>
-          <div class="price">${datas.price}</div>
-        </div>
-        <div onclick="removeItem(${id})" class="remove"><i class="fi fi-br-minus"></i></div>
-      </li>
-    `
-    return totalPrice += datas.price
-  });
-}
-function removeItem() {
-  let checkoutItem = document.querySelectorAll('ul.checkout>li')
-  checkoutItem.forEach(element => {
-    element.addEventListener('click', () => {
-      element.remove()
-    })
-  })
-}
-products.forEach((item,id) => {
+            <div class="price">${datas.price}</div>
+            </div>
+          <div onclick="removeItem(${id})" class="remove"><i class="fi fi-br-minus"></i></div>
+        </li>
+      `
+    });
+  }
+
+
   let checkoutPrice = document.querySelector('.total_price>.tprice')
   item.addEventListener('click', (e) => {
     e.stopPropagation()
@@ -56,12 +54,25 @@ products.forEach((item,id) => {
         price: +productPrice[id].innerHTML.replace(' USD',''),
         image: productImg[id].getAttribute('src')
       })
-      localStorage.setItem('checkoutList',JSON.stringify(checkoutList))
-      showCheckout(id)
-      console.log(totalPrice);
-    }
-    checkoutPrice.innerHTML = `${totalPrice} USD`
-  })
-})
 
-showCheckout() 
+      localStorage.setItem('totalPrice',+(totalPrice += (productPrice[id].innerHTML.replace(' USD',''))*1)) 
+      localStorage.setItem('checkoutList', JSON.stringify(checkoutList))
+      showCheckout()
+      checkoutPrice.innerHTML = `${localStorage.getItem('totalPrice')} USD`
+    }
+  })
+  checkoutPrice.innerHTML = `${localStorage.getItem('totalPrice')} USD`
+  return showCheckout()
+})
+function removeItem() {
+  let checkoutItem = document.querySelectorAll('ul.checkout>li')
+  checkoutItem.forEach((element,id) => {
+    element.addEventListener('click', () => {
+      element.remove()
+      checkoutList.splice(id, 1)
+      console.log(checkoutList);
+      localStorage.setItem('totalPrice', totalPrice)
+      localStorage.setItem('checkoutList', JSON.stringify(checkoutList))
+    })
+  })
+}
