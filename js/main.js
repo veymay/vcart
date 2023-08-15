c4c67327dcda5cc9ac6d80a0f3ef2eb4bfc4ee0e
VEYMAY <veymayuz@gmail.com>
2023-08-15 16:10:39 +0500

    asdasd

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@ let cart = document.querySelector('.cart')
 let overlay = document.querySelector('.overlay')
 let cartContent = document.querySelector('.cart_content')
 
-let products = document.querySelectorAll('.products>.row>.item')
-let productImg = document.querySelectorAll('.item>img')
-let productName = document.querySelectorAll('.item>.name')
-let productPrice = document.querySelectorAll('.item>.price')
 
 let checkout = document.querySelector('ul.checkout')
 
@@ -14,6 +10,52 @@ let tPrice = document.querySelector('.total_price>span.tprice')
 let checkoutList = JSON.parse(localStorage.getItem('checkoutList')) ? JSON.parse(localStorage.getItem('checkoutList')) : []
 
 let totalPrice = JSON.parse(localStorage.getItem('totalPrice')) ? JSON.parse(localStorage.getItem('totalPrice')) : 0
+
+
+function showCheckout() {
+  checkout.innerHTML = ''
+  checkoutList.forEach((datas, id) => {
+    checkout.innerHTML += `
+    <li>
+      <img src="${datas.image}" alt="">
+      <div class="data">
+        <div class="name">${datas.name}</div>
+        <div class="price">${datas.price}</div>
+      </div>
+      <div onclick="removeItem(${id})" class="remove"><i class="fi fi-br-minus"></i></div>
+    </li>`
+    tPrice.innerHTML = totalPrice + ' USD'
+  });
+}
+showCheckout()
+
+function addToCart(id) {
+  let productImg = document.querySelectorAll('.item>img')
+  let productName = document.querySelectorAll('.item>.name')
+  let productPrice = document.querySelectorAll('.item>.price')
+  checkoutList.push({
+    name: productName[id].innerHTML,
+    price: +productPrice[id].innerHTML.replace(' USD', ''),
+    image: productImg[id].getAttribute('src')
+  })
+  localStorage.setItem('checkoutList', JSON.stringify(checkoutList))
+  totalPrice += +productPrice[id].innerHTML.replace(' USD', '') 
+  localStorage.setItem('totalPrice', JSON.stringify(totalPrice))
+  tPrice.innerHTML = totalPrice + ' USD'
+  music.play()
+  showCheckout()
+}
+let products = document.querySelectorAll('.products>.row>.item')
+
+
+function removeItem(id) {
+  totalPrice -= checkoutList[id].price
+  localStorage.setItem('totalPrice', JSON.stringify(totalPrice))
+  tPrice.innerHTML = totalPrice + ' USD'
+  checkoutList.splice(id, 1)
+  localStorage.setItem('checkoutList', JSON.stringify(checkoutList))
+  showCheckout()
+}
 
 overlay.addEventListener('click', (e) => {
   e.stopPropagation()
@@ -28,84 +70,6 @@ cart.addEventListener('click', () => {
   overlay.classList.toggle('active')
 })
 
-function showCheckout() {
-  checkout.innerHTML = ''
-  checkoutList.forEach((datas, id) => {
-    checkout.innerHTML += `
-      <li>
-      <img
-          src="${datas.image}" alt="">
-        <div class="data">
-        <div class="name">${datas.name}</div>
-          <div class="price">${datas.price}</div>
-          </div>
-        <div onclick="removeItem(${id})" class="remove"><i class="fi fi-br-minus"></i></div>
-      </li>
-    `
-    tPrice.innerHTML = totalPrice + ' USD'
-  });
-}
-showCheckout()
-products.forEach((item, id) => {
-  item.addEventListener('click', (e) => {
-    e.stopPropagation()
-    if (e.target.tagName === "BUTTON") {
-      checkoutList.push({
-        name: productName[id].innerHTML,
-        price: +productPrice[id].innerHTML.replace(' USD', ''),
-        image: productImg[id].getAttribute('src')
-      })
-      localStorage.setItem('checkoutList', JSON.stringify(checkoutList))
-      showCheckout()
-      totalPrice += +productPrice[id].innerHTML.replace(' USD', '')
-      localStorage.setItem('totalPrice', JSON.stringify(totalPrice))
-      tPrice.innerHTML = totalPrice + ' USD'
-      music.play()
-    }
-  })
-})
-
-
-function removeItem(id) {
-  totalPrice -= checkoutList[id].price
-  localStorage.setItem('totalPrice', JSON.stringify(totalPrice))
-  tPrice.innerHTML = totalPrice + ' USD'
-  checkoutList.splice(id, 1)
-  localStorage.setItem('checkoutList', JSON.stringify(checkoutList))
-  showCheckout()
-}
-
-// Add Product
-
-let productList = JSON.parse(localStorage.getItem('productList')) ? JSON.parse(localStorage.getItem('productList')) : []
-
-let producContent = document.querySelector('.products>.row')
-
-let addProductImage = document.querySelector('.productImage')
-let addProductName = document.querySelector('.productName')
-let addProductPrice = document.querySelector('.productPrice')
-let addProductBtn = document.querySelector('.addProductInfo')
-
-
-addProductBtn.addEventListener('click', () => {
-  productList.push({
-    newImg: addProductImage.value,
-    title: addProductName.value,
-    price: addProductPrice.value
-  })
-  producContent.innerHTML += `
-  <div class="item">
-    <img
-      src="${addProductImage.value}">
-    <div class="name">${addProductName.value}</div>
-    <div class="price">${addProductPrice.value}</div>
-    <button>savatchaga qo'shish</button>
-  </div>
-    `
-  localStorage.setItem('productList', JSON.stringify(productList))
-})
-JSON.parse(localStorage.getItem('productList'))
-
 
 let addProduct = document.querySelector('.add')
 let music = document.querySelector('.bg_audio')
@@ -119,3 +83,39 @@ let modalContent = document.querySelector('.modal_content')
 modalContent.addEventListener('click', (e) => {
   e.stopPropagation()
 })
+
+function showProduct() {
+  productList.forEach((product, id) => {
+    productContent.innerHTML += `
+    <div class="item">
+      <img
+        src="${product.newImg}">
+      <div class="name">${product.title}</div>
+      <div class="price">${product.price}</div>
+      <button onclick='addToCart(${id})'>savatchaga qo'shish</button>
+    </div>`
+  });
+}
+
+// Add Product
+
+let productList = JSON.parse(localStorage.getItem('productList')) ? JSON.parse(localStorage.getItem('productList')) : []
+
+let productContent = document.querySelector('.products>.row')
+
+let addProductImage = document.querySelector('.productImage')
+let addProductName = document.querySelector('.productName')
+let addProductPrice = document.querySelector('.productPrice')
+let addProductBtn = document.querySelector('.addProductInfo')
+
+
+addProductBtn.addEventListener('click', () => {
+  productList.push({
+    newImg: addProductImage.value,
+    title: addProductName.value,
+    price: +addProductPrice.value
+  })
+  localStorage.setItem('productList', JSON.stringify(productList))
+  showProduct()
+})
+showProduct()
